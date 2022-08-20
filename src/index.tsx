b4fd279607e3c75ee41adf12675fc8a224cf3282
张思远 <zsy01211840@alibaba-inc.com>
2022-08-21 00:08:22 +0800

    feat: 新增登录注册 json server中间件

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import "./wdyr";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
+// import { DevTools, loadServer } from "jira-dev-tool";
+// 务必在jira-dev-tool后面引入
+import "antd/dist/antd.less";
+// import { AppProviders } from "context";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+loadDevTools(() =>
+  ReactDOM.render(
+    <React.StrictMode>
+      {/* <AppProviders> */}
+      {/* <DevTools /> */}
+      <App />
+      {/* </AppProviders> */}
+    </React.StrictMode>,
+    document.getElementById("root")
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function
